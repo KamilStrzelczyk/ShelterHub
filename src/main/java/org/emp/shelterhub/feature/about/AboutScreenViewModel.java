8ee957f8a72303ea05bc06cebfb3d57e7cd1c79e
natalia-wachulec <144425363+natalia-wachulec@users.noreply.github.com>
2025-05-20 16:07:37 +0200

    TopBar and footer (#6)

--- src/main/java/org/emp/shelterhub/feature/about/AboutScreenViewModel.java
+++ src/main/java/org/emp/shelterhub/feature/about/AboutScreenViewModel.java
@@ -4,26 +4,26 @@ import javafx.scene.layout.StackPane;
 import org.emp.shelterhub.feature.init.WelcomeScreen;
 
 public class AboutScreenViewModel {
-    State state = new State();
-    private StackPane root;
+  State state = new State();
+  private StackPane root;
 
-    public AboutScreenViewModel(StackPane root) {
-        this.root = root;
-    }
+  public AboutScreenViewModel(StackPane root) {
+    this.root = root;
+  }
 
-    void setRoot(StackPane root) {
-        this.root = root;
-    }
+  void setRoot(StackPane root) {
+    this.root = root;
+  }
 
-    void navigateToWelcomeScreen() {
-        if (root != null) {
-            root.getChildren().clear();
-            WelcomeScreen.show(root);
-        }
+  void navigateToWelcomeScreen() {
+    if (root != null) {
+      root.getChildren().clear();
+      WelcomeScreen.show(root);
     }
+  }
 }
 
 class State {
-    String name;
-    String password;
+  String name;
+  String password;
 }
